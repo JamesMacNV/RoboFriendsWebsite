f780272787b4f598f944151fd088b9a4a10c516c
JamesMacNV <36140375+JamesMacNV@users.noreply.github.com>
2019-08-02 16:22:18 -0700

    Added sticky scroll bar. Organized code.

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
 import './App.css'
+import Scroll from '../Components/Scroll'
 
 class App extends Component{
 	constructor() {
@@ -26,14 +27,17 @@ class App extends Component{
 	}
 
 	render(){
-		const filteredRobots = this.state.robots.filter(robots => {
-		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const {robots, searchfield }= this.state;
+		const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 	   })
 		return (
 		<div className='tc'>
 			<h1 className='f1'>RoboFriends </h1>
 			<SearchBox searchChange={this.onSearchChange} />
+			<Scroll>
 			<CardList robots={filteredRobots}/>
+			</Scroll>
 		</div>	
 
 		);
